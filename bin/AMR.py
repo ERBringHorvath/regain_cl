3c989fd86c02815a3d0f07dea8a6ead4a7af1f1e
Eli Bring Horvath <97261650+ERBringHorvath@users.noreply.github.com>
2024-02-22 17:13:07 -0700

    Update AMR.py

--- bin/AMR.py
+++ bin/AMR.py
@@ -1,3 +1,30 @@
+"""
+    Identifies resistance and virulence genes from FASTA files using the AMRfinderPlus tool.
+    
+    This function is part of the ReGAIN suite, designed to analyze genomic sequences to detect
+    antimicrobial resistance (AMR) and virulence factors. Users can specify an organism to target
+    the analysis or conduct a non-specific search across multiple organisms. The function supports
+    multi-threading to expedite analysis.
+
+    This module utilizes the NCBI AMRfinderPlus Software:
+    Feldgarden M, Brover V, Gonzalez-Escalona N, et al. 
+    AMRFinderPlus and the Reference Gene Catalog facilitate examination of the genomic links among antimicrobial resistance, stress response, and virulence. 
+    Sci Rep. 2021;11(1):12728. Published 2021 Jun 16. doi:10.1038/s41598-021-91456-0
+    
+    Parameters:
+        directory (str): Path to the directory containing FASTA files for analysis.
+        organism (str, optional): Specific organism to analyze. If None, analysis is non-specific.
+        threads (int, optional): Number of CPU cores to dedicate to the analysis. Default is 1.
+        output_dir (str): Directory to store the AMRfinder results.
+        
+    Note:
+        ReGAIN and its analytical modules are under US provisional patent 63/526,656. Users are encouraged
+        to consult the patent status for terms of use and licensing information.
+    
+    Returns:
+        None
+"""
+
 import os
 import argparse
 from tqdm import tqdm
