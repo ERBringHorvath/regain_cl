c07248d28eed92b7a1c08969d6e9490df4d3555d
Eli Bring Horvath <97261650+ERBringHorvath@users.noreply.github.com>
2024-03-09 21:19:38 -0700

    Update AMR.py

--- bin/AMR.py
+++ bin/AMR.py
@@ -74,6 +74,8 @@ def run(args):
     if args.threads:
         threads_flag = f"--threads {args.threads}"
         print(f"\033[36m Using {args.threads} cores.\n" + "\033[0m")
+    else:
+        print("\033[36m Using 1 core...\n" + "\033[0m")
 
     output_directory = os.path.join(directory, "AMRfinder_Results")
     if args.output_dir:
